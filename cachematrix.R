d62c2f29cc09eeeff8aedd6b7e66a47ba846d357
Yung Lin <yungclin@gmail.com>
2014-05-19 23:04:39 -0700

    finished assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,76 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## The purpose is to use scoping rules to cache the inverse matrix from a matrix created using a parent function.
 
-## Write a short comment describing this function
+## Creates a list of 4 functions.  1. Set Matrix funciton 2. Get Matrix Function 3. Set Inverse of Set Matrix 4. Get Inverse of Set Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(mean) m <<- mean
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Function will output the cache of inverse matrix if available instead of calculating everytime
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        return(m)
+}
+
+
+############## ORIGINAL R CODE PROVIDED #########################
+
+
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+##The following function calculates the mean of the special "vector" created with the above function. 
+##However, it first checks to see if the mean has already been calculated. 
+##If so, it gets the mean from the cache and skips the computation. 
+##Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
